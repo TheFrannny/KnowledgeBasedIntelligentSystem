68f161dcb69513c4a981f9608e4cac03f646fe0c
Fran <979103+TheFrannny@users.noreply.github.com>
2022-03-15 15:05:54 -0400

    Imported GTK along with copying a GUI for testing purposes. Made a list of things to do for the project

--- cmake-build-debug/CMakeFiles/3.21.1/CMakeCCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.21.1/CMakeCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_COMPILER "C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/bin/gcc.exe")
+set(CMAKE_C_COMPILER "C:/msys64/mingw64/bin/cc.exe")
 set(CMAKE_C_COMPILER_ARG1 "")
 set(CMAKE_C_COMPILER_ID "GNU")
 set(CMAKE_C_COMPILER_VERSION "11.2.0")
@@ -20,11 +20,11 @@ set(CMAKE_C_SIMULATE_VERSION "")
 
 
 
-set(CMAKE_AR "C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/bin/ar.exe")
-set(CMAKE_C_COMPILER_AR "C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/bin/gcc-ar.exe")
-set(CMAKE_RANLIB "C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/bin/ranlib.exe")
-set(CMAKE_C_COMPILER_RANLIB "C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/bin/gcc-ranlib.exe")
-set(CMAKE_LINKER "C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/bin/ld.exe")
+set(CMAKE_AR "C:/msys64/mingw64/bin/ar.exe")
+set(CMAKE_C_COMPILER_AR "C:/msys64/mingw64/bin/gcc-ar.exe")
+set(CMAKE_RANLIB "C:/msys64/mingw64/bin/ranlib.exe")
+set(CMAKE_C_COMPILER_RANLIB "C:/msys64/mingw64/bin/gcc-ranlib.exe")
+set(CMAKE_LINKER "C:/msys64/mingw64/bin/ld.exe")
 set(CMAKE_MT "")
 set(CMAKE_COMPILER_IS_GNUCC 1)
 set(CMAKE_C_COMPILER_LOADED 1)
@@ -74,7 +74,7 @@ endif()
 
 
 
-set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include;C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/include;C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include-fixed;C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/x86_64-w64-mingw32/include")
-set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "mingw32;gcc;moldname;mingwex;kernel32;pthread;advapi32;shell32;user32;kernel32;iconv;mingw32;gcc;moldname;mingwex;kernel32")
-set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0;C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/lib/gcc;C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/x86_64-w64-mingw32/lib;C:/Program Files/JetBrains/CLion 2021.3.3/bin/mingw/lib")
+set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/11.2.0/include;C:/msys64/mingw64/include;C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/11.2.0/include-fixed;C:/msys64/mingw64/x86_64-w64-mingw32/include")
+set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "mingw32;gcc;moldname;mingwex;kernel32;pthread;advapi32;shell32;user32;kernel32;mingw32;gcc;moldname;mingwex;kernel32")
+set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/11.2.0;C:/msys64/mingw64/lib/gcc;C:/msys64/mingw64/x86_64-w64-mingw32/lib;C:/msys64/mingw64/lib")
 set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
--- main.cpp
+++ main.cpp
@@ -1,17 +1,52 @@
 #include <iostream>
+#include <gtk/gtk.h>
+using namespace std;
+
 /**
  * AI project 3
  * Francisco Romero, William Daniel Hiromoto, Nolan Boner
  * The goal of this project is to design and build a Knowledge-Based Intelligent System that collects user preferences and reasons about them.
- * Featuring an easy-to-use GUI for collecting, from the user, names of attributes and their values, hard con-straints, and preferences.
+ * Featuring an easy-to-use GUI for collecting, from the user, names of attributes and their values, hard constraints, and preferences.
  * This program will also be able to read inputs from a file.
  */
 
 /*
- atributes in binary
+    todo::atributes in binary
 
- */
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    todo::Hard constraints (H) are represented as propositional formulas in the Conjunc-tional Normal Form (CNF)
+
+    todo::preferences (T ) in the preference languages of penalty logic, possibilistic logic, and qualitative choice logic.
+        Formulas involved in the preference theories (i.e., the φ’s and the ψ’s) are of CNF as well.
+
+    todo::Existence of feasible objects: decide whether there are feasible objects w.r.t H,
+        that is, whether there are models of H that are truth assignments making H true.
+
+    todo::Exemplification: generate, if possible, two random feasible objects, and show the
+        preference between the two (strict preference or equivalence) w.r.t T .
+
+    todo::Optimization: find an optimal object w.r.t T .
+
+    todo::Omni-optimization: find all optimal objects w.r.t T
+*/
+
+// borrowing https://fodor.org/blog/cpp-gtk4-gui/
+static void on_activate (GtkApplication *app) {
+    // Create a new window
+    GtkWidget *window = gtk_application_window_new (app);
+    // Create a new button
+    GtkWidget *button = gtk_button_new_with_label ("Hello, World!");
+    // When the button is clicked, close the window passed as an argument
+    g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_close), window);
+    gtk_window_set_child (GTK_WINDOW (window), button);
+    gtk_window_present (GTK_WINDOW (window));
 }
+
+int main(int argc, char **argv) {
+    // Create a new application
+    cout << "reeee" << endl;
+    GtkApplication *app = gtk_application_new ("com.example.GtkApplication",
+                                               G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (on_activate), NULL);
+    return g_application_run (G_APPLICATION (app), argc, argv);
+}
+
