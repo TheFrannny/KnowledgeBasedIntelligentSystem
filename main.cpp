76866f84b61a500096ea8e771890a88861708cd2
Fran <TheFranciscoRomero@gmail.com>
2022-03-15 13:45:34 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/**
+ * AI project 3
+ * Francisco Romero, William Daniel Hiromoto, Nolan Boner
+ * The goal of this project is to design and build a Knowledge-Based Intelligent System that collects user preferences and reasons about them.
+ * Featuring an easy-to-use GUI for collecting, from the user, names of attributes and their values, hard con-straints, and preferences.
+ * This program will also be able to read inputs from a file.
+ */
+
+/*
+ atributes in binary
+
+ */
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
